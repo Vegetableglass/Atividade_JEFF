71bde456fca50e5146fb487d0517283ab08f61b7
Vegetableglass <kaiosouto10@gmail.com>
2025-05-28 17:34:41 -0300

    :sparkles: feat : Adicionando o historico.

--- calculadora.py
+++ calculadora.py
@@ -1,20 +1,29 @@
 rodar = True
-while rodar == True:
-        
+Historico = []  
+while rodar == True: 
     try:
         def soma(a,b):
             resultado = a + b
             print(f"{50*"-"}\nO resultado da soma entre {a} e {b} é {resultado}")
+            Historico.append(f"{resultado} = {a} + {b}")
 
         def sub(a,b):
             resultado = a - b
             print(f"{50*"-"}\nO resultado da subtração entre {a} e {b} é {resultado}")
+            Historico.append(f"{resultado} = {a} - {b}")
 
         def mult(a,b):
             resultado = a * b
             print(f"{50*"-"}\nO resultado da multiplicação entre {a} e {b} é {resultado}")
+            Historico.append(f"{resultado} = {a} * {b}")
+        
+        def hist():
+            if len(Historico) > 0:
+                print(Historico)
+            else:
+                print(f"{50*"-"}\nHistorico vazio.")    
             
-        operacao = (int(input(f"{50*"-"}\nEscolha uma operação:\n1:Soma\n2:Subtração\n3:Multiplicação\n4:Para sair dessa calculadora\nDigite aqui:")))
+        operacao = (int(input(f"{50*"-"}\nEscolha uma operação:\n1:Soma\n2:Subtração\n3:Multiplicação\n4:Para ver historico\n5:Para sair dessa calculadora\nDigite aqui:")))
         if operacao in [1,2,3]:
                 a = (float(input("Escolha um número para realizar a operações(Digite números decimais com . ao inves de ,):")))
                 b = (float(input("Escolha outro número para realizar a operações(Digite números decimais com . ao inves de ,):")))
@@ -29,6 +38,9 @@ while rodar == True:
                     mult(a,b)    
                 
         elif operacao == 4:
+            hist()
+
+        elif operacao == 5:
             rodar = False
             print(f"{50*"-"}\nPrograma encerrado.\n{50*"-"}")
 
