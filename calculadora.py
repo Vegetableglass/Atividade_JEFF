46b0e8fcee7afd8f4da5058f44d74151fe0ab499
JoseCaioLR <caiolinh11@gmail.com>
2025-06-08 12:02:13 -0300

    Adicionando sistema de pontuação

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,8 @@
 import random, operator
 rodar = True
-Historico = []  
+Historico = []
+Pontos = [0]
+Partidas_jogadas = [0]
 while rodar == True: 
     try:
         def soma(a,b):
@@ -74,11 +76,19 @@ while rodar == True:
             resposta = float(input(f"{50*"-"}\nResolva: {va} {op} {vb}\n{50*"-"}\nDigite aqui(Apenas 2 casas decimais serão consideradas após o ponto, e é necessário arredondar):"))
             resposta_certa = round(opf[op](va,vb),2)
             if resposta == resposta_certa:
-                print(f"{50*"-"}\nVocê acertou!")
+                print(f"{50*"-"}\nVocê acertou e ganhou 1 ponto!")
+                Pontos[0] += 1
+                Partidas_jogadas[0] += 1
             else:
-                print(f"{50*"-"}\nVocê errou...O resultado era {resposta_certa}.") 
+                print(f"{50*"-"}\nVocê errou e não ganhou nenhum ponto...O resultado era {resposta_certa}.")
+                Partidas_jogadas[0] += 1
+
+        def verp():
+            pontos = Pontos[0]
+            partidas = Partidas_jogadas[0]
+            print(f"{50*"-"}\nVocê tem {pontos} ponto(s) e {partidas} partida(s) jogada(s).")
             
-        operacao = int(input(f"{50*"-"}\nEscolha uma operação:\n1:Soma\n2:Subtração\n3:Multiplicação\n4:Divisão\n5:Potência\n6:Resto da Divisão\n7:Historico\n8:Limpar historico\n9:Jogo da calculadora inversa!\n10:Para sair da calculadora\nDigite aqui:"))
+        operacao = int(input(f"{50*"-"}\nEscolha uma operação:\n1:Soma\n2:Subtração\n3:Multiplicação\n4:Divisão\n5:Potência\n6:Resto da Divisão\n7:Historico\n8:Limpar historico\n9:Jogo da calculadora inversa!\n10:Ver pontos da calculadora inversa\n11:Para sair da calculadora\nDigite aqui:"))
         if operacao in [1,2,3,4,5,6]:
                 a = float(input("Escolha um número para realizar a operações(Digite números decimais com . ao inves de ,):"))
                 b = float(input("Escolha outro número para realizar a operações(Digite números decimais com . ao inves de ,):"))
@@ -111,6 +121,9 @@ while rodar == True:
             ci()
 
         elif operacao == 10:
+            verp()
+
+        elif operacao == 11:
             rodar = False
             print(f"{50*"-"}\nPrograma encerrado.\n{50*"-"}")
 
